b28fe31401f9619aa5bf543b5c11a645ec4d3c18
Steve Strong <steve@srstrong.com>
2022-11-28 22:03:53 +0000

    wip on new

--- new/common/home-configuration.nix
+++ new/common/home-configuration.nix
@@ -1,23 +1,64 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 let
   foo = "bar";
 in
-  nixpkgs.overlays = [
-    emacs.overlay
-  ];
+{
+  home.stateVersion = "23.05";
 
-  home-manager.users.steve = {
-    programs.emacs.enable = true;
-    programs.emacs.package =
-      (
-        pkgs.emacsWithPackagesFromUsePackage {
-          alwaysEnsure = true;
-          alwaysTangle = true;
+    home.packages = with pkgs; [
+      alacritty
+      aspell
+      aspellDicts.en
+      aspellDicts.en-computers
+      autoconf
+      #awscli2
+      bat
+      bc
+      clang
+      coreutils
+      diff-so-fancy
+      duf
+      fd
+      (ffmpeg-full.override {game-music-emu = null;})
+      fx
+      fzf
+      git-lfs
+      git-filter-repo
+      gnumake
+      gnupg
+      gnused
+      htop
+      iftop
+      influxdb
+      ipcalc
+      jq
+      kitty
+      lorri
+      ncurses6
+      nix-prefetch-git
+      nmap
+      #nginx
+      python3
+      ripgrep
+      rsync
+      silver-searcher
+      ssm-session-manager-plugin
+      up
+      websocat
+      wget
+      youtube-dl
+    ];
 
-          # Custom overlay derived from 'emacs' flake input
-          package = pkgs.emacs;
-          config = "";
-              }
-      );
-  };
+  programs.emacs.enable = true;
+  programs.emacs.package = 
+    (
+      pkgs.emacsWithPackagesFromUsePackage {
+        alwaysEnsure = true;
+        alwaysTangle = true;
+
+        # Custom overlay derived from 'emacs' flake input
+#        package = pkgs.emacs;
+        config = "";
+            }
+    );
 }
--- new/macbookM1.sh
+++ new/macbookM1.sh
@@ -1,2 +1,4 @@
+set -e
+
 nix build --impure .\#darwinConfigurations.macbookM1.system
-#./result/sw/bin/darwin-rebuild switch --flake .#macbookM1
+./result/sw/bin/darwin-rebuild switch --flake .#macbookM1
