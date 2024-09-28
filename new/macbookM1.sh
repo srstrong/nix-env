4ce4dcf05c0d027fb131c55cd4fd57dc74ca4d64
Steve Strong <steve@srstrong.com>
2024-09-28 08:26:35 +0100

    more cleanup

--- new/macbookM1.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-nix --extra-experimental-features flakes --extra-experimental-features nix-command build --impure .\#darwinConfigurations.macbookM1.system
-./result/sw/bin/darwin-rebuild switch --flake .#macbookM1
